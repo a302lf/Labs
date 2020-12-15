60edc97ef26b09807630c4d8488a23025cf17c5e
a302lf <71495392+a302lf@users.noreply.github.com>
2020-12-15 04:37:39 +0300

    Add files via upload

---
+++ 1.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k;
+	cin >> k;
+	cout << k << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ 1.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Input number a=";
+	cin >> a; 
+	cout << "Input number b=";
+	cin  >> b;
+	cout << a << "+"<< b << "=" << a+b << endl;
+	system("pause");
+	return 0;
+}
